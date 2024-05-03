3cb8c3f9fdf04e18283528e52568074a6257f09f
Mihaltsow Aleksei <michalcow99@mail.ru>
2024-05-03 17:50:48 +0300

    add seven task

--- newSkills.py
+++ newSkills.py
@@ -130,3 +130,18 @@
 # anagrams = ['aabb', 'abcd', 'bbaa', 'dada']
 #
 # print(filter_anagrams(word, anagrams))
+
+
+# def likes(names):
+#     if len(names) == 0:
+#         return 'Никто не оценил данную запись'
+#     elif len(names) == 1:
+#         return f'{names[0]} оценил(a) данную запись'
+#     elif len(names) == 2:
+#         return f'{names[0]} и {names[1]} оценили данную запись'
+#     elif len(names) == 3:
+#         return f'{names[0]}, {names[1]} и {names[2]} оценили данную запись'
+#     return f'{names[0]}, {names[1]} и {len(names) - 2} других оценили данную запись'
+#
+#
+# print(likes(['Том']))

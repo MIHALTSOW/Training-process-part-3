d8aee7d700b1d3fbc5b6af77627e7230f56117c9
Mihaltsow Aleksei <michalcow99@mail.ru>
2024-05-03 12:58:14 +0300

    add first task

--- newSkills.py
+++ newSkills.py
@@ -1,2 +1,20 @@
-for i in range(10):
-    print('Hello world!')
+# Part 2.1
+# def hide_card(card_number):
+#     card_number = card_number.replace(' ', '')
+#     hidden_number = ''
+#     for idx, digit in enumerate(card_number):
+#         if idx < 12:
+#             hidden_number += '*'
+#         else:
+#             hidden_number += digit
+#     return hidden_number
+#
+#
+# print(hide_card(input()))
+#
+#
+# # more comfortable solution
+# def hide_card(card_number):
+#     return '*' * 12 + card_number.replace(' ', '')[-4:]
+
+

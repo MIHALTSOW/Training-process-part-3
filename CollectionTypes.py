87b95ee91105ac410c1c24d3ad94582601aeb723
Mihaltsow Aleksei <michalcow99@mail.ru>
2024-05-24 14:14:24 +0300

    add modules 6.1 and 6.2

---
+++ CollectionTypes.py
@@ -0,0 +1,17 @@
+# teacher solution
+
+# from string import ascii_letters
+#
+# translator = str.maketrans(ascii_letters, input() * 2)
+#
+# print(input().translate(translator))
+
+# my solution
+
+# new = input() * 2
+# old = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ'
+#
+# translation_table = str.maketrans(old, new)
+# original_string = input()
+#
+# print(original_string.translate(translation_table))
